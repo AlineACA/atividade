58dd33a1e4cf9b69396cf13f15171fc18b50f436
Aline Coutinho <169111822+AlineACA@users.noreply.github.com>
2024-10-25 12:03:23 -0300

    Create Classificador.js

---
+++ Classificador.js
@@ -0,0 +1,26 @@
+
+let nomeHero = "Mario";
+let xpHero = -10;
+
+
+let nivel;
+if (xpHero < 1000) {
+  nivel = "Ferro";
+} else if (xpHero <= 2000) {
+  nivel = "Bronze";
+} else if (xpHero <= 5000) {
+  nivel = "Prata";
+} else if (xpHero <= 7000) {
+  nivel = "Ouro";
+} else if (xpHero <= 8000) {
+  nivel = "Platina";
+} else if (xpHero <= 9000) {
+  nivel = "Ascendente";
+} else if (xpHero <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+
+console.log("O herói, " + nomeHero + "!" + " está no nível de " + nivel+ "!");
