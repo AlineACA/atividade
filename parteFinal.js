ecb0f4cc66f69fbef463e296d6946f838f51d2ff
Aline Coutinho <169111822+AlineACA@users.noreply.github.com>
2024-11-04 21:30:06 -0300

    Update parteFinal.js

--- parteFinal.js
+++ parteFinal.js
@@ -1,1 +1,34 @@
 
+class Heroi {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo; 
+    }
+  
+    atacar() {
+      const ataques = {
+        mago: 'magia',
+        guerreiro: 'espada',
+        monge: 'artes marciais',
+        ninja: 'shuriken',
+      };
+  
+      const ataque = ataques[this.tipo] || 'ataque desconhecido'; 
+  
+      console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`); 
+    }
+  }
+  
+  // Exemplos de uso:
+  const heroi1 = new Heroi('Palkia', 100, 'mago');
+  heroi1.atacar();
+  
+  const heroi2 = new Heroi('Ragnar', 30, 'guerreiro');
+  heroi2.atacar();
+  
+  const heroi3 = new Heroi('Tenzin', 50, 'monge');
+  heroi3.atacar(); 
+  
+  const heroi4 = new Heroi('Satoshi', 25, 'ninja');
+  heroi4.atacar(); 
